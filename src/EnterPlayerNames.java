7b89ebd53dec5e760e4aa78ee567ead87ae04bc2
Mark2k21.taekwondoman24@gmail.com <Mark2k21.taekwondoman24@gmail.com>
2021-10-16 16:32:59 -0500

    Two Screens work

--- src/EnterPlayerNames.java
+++ src/EnterPlayerNames.java
@@ -5,25 +5,28 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
 
 public class EnterPlayerNames {//controller for EnterPlayerNames.fxml
 
     @FXML
     private Pane EnterPlayerNamePane;
 
+//    @FXML private Button saveButton;
+    //OnAction: saveButtonPressed
+    //fxID: saveButton
+    
+    
+//    @FXML private Button continueButton;
+    //OnAction: continueButtonPressed?
+    //fxID: continueButton
+    
     @FXML
-    private Button saveButton;
+   private Text pidTextBox;
 
-    @FXML
-    private Button continueButton;
+ //   @FXML void continueButtonPressed(ActionEvent event) {
 
-    @FXML
-    private TextField pidTextBox;
-
-    @FXML
-    void continueButtonPressed(ActionEvent event) {
-
-    }
+ //   }
 
     @FXML
     void saveButtonPressed(ActionEvent event) {
--- src/SceneController.java
+++ src/SceneController.java
@@ -18,14 +18,7 @@ public class SceneController {
 	private Scene scene;
 	private Parent root;
 	
-	public void saveButtonPressed(ActionEvent event) throws IOException {
-		Parent root = FXMLLoader.load(getClass().getResource("Uno.fxml"));
-		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-		scene = new Scene(root);
-		stage.setScene(scene);
-		stage.show();
-	}
-	
+
 	public void startButtonLoadNameScreen(ActionEvent event) throws IOException {//surround with try and catch block is what IOException does
 		Parent root = FXMLLoader.load(getClass().getResource("EnterPlayerNames.fxml"));
 		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
@@ -34,4 +27,11 @@ public class SceneController {
 		stage.show();
 	}
 
+	public void saveButtonPressed(ActionEvent event) throws IOException {
+		Parent root = FXMLLoader.load(getClass().getResource("GameBoard.fxml"));
+		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+		scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
+	}
 }
