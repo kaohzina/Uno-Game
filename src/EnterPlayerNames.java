2d6f8552d123c91d0ff73c2702db045555186725
Mark2k21.taekwondoman24@gmail.com <Mark2k21.taekwondoman24@gmail.com>
2021-10-17 11:32:39 -0500

    most up to date work

--- src/EnterPlayerNames.java
+++ src/EnterPlayerNames.java
@@ -1,8 +1,10 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.MouseEvent;
+import java.util.ArrayList;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
 import javafx.scene.text.Text;
@@ -22,15 +24,64 @@ public class EnterPlayerNames {//controller for EnterPlayerNames.fxml
     //fxID: continueButton
     
     @FXML
-   private Text pidTextBox;
+   private TextField pidTextBox;
 
- //   @FXML void continueButtonPressed(ActionEvent event) {
+    //Creating arraylist for users names to be input as players 1-4
+    ArrayList<String> playerIds = new ArrayList<String>();
+	//Name Spaces on "Enter Player Names" slide.
+    
+    public String[] getPlayerIDs() {//takes arraylist playerIDs created above, converting to array, making sure array size fits. 
+    	String[] PlayerIDs = playerIds.toArray(new String[playerIds.size()]);
+    	return PlayerIDs;
+    }
+    
+    @FXML private Text pidOneLabel;
+     
+   // playerIds.add();
+    
+    @FXML private Text pidTwoLabel;
 
- //   }
+    @FXML private Text pidThreeLabel;
 
+    @FXML private Text pidFourLabel;
+    
+    
     @FXML
-    void saveButtonPressed(ActionEvent event) {
-
+    void saveButtonPressed(ActionEvent event) {//LIKELY NEEDS TO BE REDONE -- COME BACK TO
+		//Is textbox empty?
+		Label labelresponse = new Label();
+		TextField text = new TextField();
+		
+		if (text.getText().isEmpty()) 
+		{
+			labelresponse.setText("Please enter a name.");
+		}
+		else 
+		{
+			String name = pidTextBox.getText();
+			playerIds.add(name);
+			
+			if(playerIds.size() == 1) {//Likely needs to be redone - COME BACK TO
+				pidOneLabel.setText(playerIds.get(0));
+			}
+			else if(playerIds.size() == 2) {
+				pidTwoLabel.setText(playerIds.get(1));
+			}
+			else if(playerIds.size() == 3) {
+				pidThreeLabel.setText(playerIds.get(2));
+			}
+			else if(playerIds.size() == 4) {
+				pidFourLabel.setText(playerIds.get(3));
+			}
+			if(playerIds.size() < 0 || playerIds.size() < 5) {//CHANGE TO ALERT WARNING?!?!
+				throw new IllegalArgumentException("Please enter a name."); //might have to alter this into warning message
+			}
+			
+			
+		}
+		
+		
+		
     }
 
     @FXML
--- src/SceneController.java
+++ src/SceneController.java
@@ -1,9 +1,13 @@
 import java.io.IOException;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.scene.Node;
 
@@ -18,16 +22,27 @@ public class SceneController {
 	private Scene scene;
 	private Parent root;
 	
-
-	public void startButtonLoadNameScreen(ActionEvent event) throws IOException {//surround with try and catch block is what IOException does
+	//Uno Main Menu "Start Button" functionality
+	@FXML public void startButtonLoadNameScreen(ActionEvent event) throws IOException {//surround with try and catch block is what IOException does
 		Parent root = FXMLLoader.load(getClass().getResource("EnterPlayerNames.fxml"));
 		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
 		scene = new Scene(root);
 		stage.setScene(scene);
 		stage.show();
 	}
+	//Uno Main Menu "Exit Button" functionality
+	@FXML public void exitButton(ActionEvent event) throws IOException {
+		System.exit(0); //WORKING ON SETTING ALERT CONFIRMATION
+	}
+	
+	//Enter Player Names "Add/Save Names Button" functionality
+	@FXML public void saveButtonPressed(ActionEvent event) throws IOException {//This is not working
 
-	public void saveButtonPressed(ActionEvent event) throws IOException {
+		
+	}
+	
+	//Enter Player Names "Continue Button" functionality
+	@FXML public void continueButtonPressed(ActionEvent event) throws IOException {
 		Parent root = FXMLLoader.load(getClass().getResource("GameBoard.fxml"));
 		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
 		scene = new Scene(root);
@@ -35,22 +50,14 @@ public class SceneController {
 		stage.show();
 	}
 	
-	public void continueButtonPressed(ActionEvent event) throws IOException {
-		Parent root = FXMLLoader.load(getClass().getResource("GameBoard.fxml"));
-		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-		scene = new Scene(root);
-		stage.setScene(scene);
-		stage.show();
-	}
-	
-	public void exitButton(ActionEvent event) throws IOException {
-		System.exit(0);
+	//GameBoard "Exit Button" functionality
+	@FXML public void gameBoardExit(ActionEvent event) throws IOException {
+		System.exit(0); //WORKING ON SETTING ALERT CONFIRMATION
 	}
 	
 	
-	
-	
-	
+    
+
 	
 	
 	
