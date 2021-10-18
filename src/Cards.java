57a811edbd5c420e6624f45d5393dc8365f22b19
Xiong Yang <yangx04@uwgb.edu>
2021-10-18 15:00:32 -0500

    working cards

--- src/Cards.java
+++ src/Cards.java
@@ -3,13 +3,49 @@
 public class Cards {
 	public static void main(String[] args) {
 		
+		Colors[] colors = Colors.values(); {
 		
-		
-		System.out.print(Colors.colors[0]);
-		System.out.print(Values.values[0]);
-		
-		
+		for (int i = 0; i < colors.length - 1; i++) {
+			Colors color = colors[i];
 	
+			
+		Values[] values = Values.values(); {
+		
+			for (int k = 0; k < 1; k++) {
+				Values value = values[k];
+				
+				System.out.printf("%s %s, %n", color, value);
+				
+			}
+			
+			for (int j = 1; j < 10; j++) {
+				Values value = values[j];
+				
+				System.out.printf("%s %s, %n", color, value);
+				System.out.printf("%s %s, %n", color, value);
+				
+			}
+			
+			for (int s = 10; s < 13; s++) {
+				Values special = values[s];
+				
+				System.out.printf("%s %s, %n", color, special);
+				System.out.printf("%s %s, %n", color, special);
+				
+			}
+			for (int s = 13; s < 15; s++) {
+				Values special = values[s];
+				
+				System.out.printf("%s %s, %n", color, special);
+				
+				
+			}
+		}
+			
+		}
+		}
+		
+		
 	}
 	
 public enum Colors { Red, Blue, Green, Yellow, Wild;
