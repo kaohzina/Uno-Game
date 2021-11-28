8563f155c5340164a94f6cd53231ce6e0ef01d45
kaohzina <zinanshin.kaoher@gmail.com>
2021-11-28 16:55:54 -0600

    adjustments

--- src/EnterPlayerNames.java
+++ src/EnterPlayerNames.java
@@ -11,13 +11,11 @@ import javafx.scene.text.Text;
 
 public class EnterPlayerNames {//controller for EnterPlayerNames.fxml
 
-    @FXML
-    private Pane EnterPlayerNamePane;
+    @FXML private Pane EnterPlayerNamePane;
 
 //    @FXML private Button saveButton;
     //OnAction: saveButtonPressed
     //fxID: saveButton
-    
     
     @FXML private Button continueButton; //will transfer to game board
     //OnAction: continueButtonPressed?
@@ -26,11 +24,11 @@ public class EnterPlayerNames {//controller for EnterPlayerNames.fxml
 //    @FXML
 //   private TextField pidTextBox;
 
-    //Creating arraylist for users names to be input as players 1-4
+    //Creating array list for users names to be input as players 1-4
     ArrayList<String> playerIds = new ArrayList<String>();
 	//Name Spaces on "Enter Player Names" slide.
     
-    public String[] getPlayerIDs() {//takes arraylist playerIDs created above, converting to array, making sure array size fits. 
+    public String[] getPlayerIDs() {//takes array list playerIDs created above, converting to array, making sure array size fits. 
     	String[] PlayerIDs = playerIds.toArray(new String[playerIds.size()]);
     	return PlayerIDs;
     }
@@ -38,7 +36,7 @@ public class EnterPlayerNames {//controller for EnterPlayerNames.fxml
 
     @FXML
     void savePidMouseClick(MouseEvent event) {
-
+    	
     }
     
     @FXML void continueButtonPressed(ActionEvent event) {
